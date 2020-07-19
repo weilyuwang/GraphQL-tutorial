64d6ae346f705fa3dd3ef793a79f29d8af04e248
weilyuwang <weilyuwang@gmail.com>
2020-07-19 16:26:39 -0400

    :fire: setup author data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,9 @@
 const {
-  graphql,
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
+  GraphQLInt,
 } = require("graphql");
 
 const _ = require("lodash");
@@ -15,12 +15,27 @@ let books = [
   { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
 ];
 
+let authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" },
+];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -37,6 +52,18 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db / other source
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return _.find(authors, { id: args.id });
       },
     },
   },
