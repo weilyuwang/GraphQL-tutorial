422bd751dfa9a0eacfab601c4f78dc18f060eb61
weilyuwang <weilyuwang@gmail.com>
2020-07-19 17:14:38 -0400

    :art: graphql List

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,6 +4,7 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
 } = require("graphql");
 
 const _ = require("lodash");
@@ -13,6 +14,9 @@ let books = [
   { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
   { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantasic", genre: "Fantasy", id: "6", authorId: "3" },
 ];
 
 let authors = [
@@ -43,6 +47,12 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
