cf6e27b047dd9b442e5096010cf931076c81dc97
weilyuwang <weilyuwang@gmail.com>
2020-07-19 16:34:58 -0400

    :zap: type relations between author and book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,9 @@ const _ = require("lodash");
 
 // dummy data
 let books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
 ];
 
 let authors = [
@@ -27,6 +27,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        // parent is the Book object
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
   }),
 });
 
