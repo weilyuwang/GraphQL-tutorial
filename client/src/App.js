26a6b9ab2fb67e564da18bec7d253a21dbdb0d22
weilyuwang <weilyuwang@gmail.com>
2020-07-20 15:05:36 -0400

    :art: some stylings and booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function App() {
+// Components
+import BookList from "./components/BookList";
+
+const App = () => {
   return (
     <div id="main">
       <h1>Reading List</h1>
+      <BookList />
     </div>
   );
-}
+};
 
 export default App;
