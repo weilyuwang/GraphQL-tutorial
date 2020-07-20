e00f590214efa13bc74cdfb8124234f76dc09625
weilyuwang <weilyuwang@gmail.com>
2020-07-20 18:08:50 -0400

    :ambulance: apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 
 // Components
 import BookList from "./components/BookList";
 
+// Apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
 const App = () => {
   return (
-    <div id="main">
-      <h1>Reading List</h1>
-      <BookList />
-    </div>
+    // Pass Apollo Client into ApolloProvider
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 };
 
